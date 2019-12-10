2b3b9fccfba1974551849b5fb7695ba0dcb2a61b
Junxiong.Xu <Junxiong.Xu@geely.com>
2019-12-10 14:33:00 +0800

    冒泡排序&&防抖

--- debounce/index.js
+++ debounce/index.js
@@ -1,4 +1,26 @@
 // 防抖 debounce
-
+function debounce (fn, delay, immediate) {
+  let timer
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer)
+    } else {
+     timer =  setTimeout(() => {
+        fn.apply(this, args)
+      }, delay)
+    }
+  }
+}
 
 // 节流 throttle
+function throttle (fn, delay) {
+  let timer
+  return (...args) => {
+    if (!timer) {
+      timer = setTimeout(() => {
+        timer = null
+        fn.apply(this, args)
+      }, delay)
+    }
+  }
+}
